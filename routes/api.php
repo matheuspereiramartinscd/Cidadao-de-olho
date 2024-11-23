db3315a8e843cbf854b7e229582c570c056d8243
Matheus <matheuspereiramartins1993@gmail.com>
2024-11-23 18:35:08 -0300

    another change

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\DeputadoController;
-
-Route::get('/ranking-reembolsos', [DeputadoController::class, 'rankingReembolsos']);
-Route::get('/ranking-redes-sociais', [DeputadoController::class, 'rankingRedesSociais']);
-Route::get('/get-deputados', [DeputadoController::class, 'getDeputados']);
-
